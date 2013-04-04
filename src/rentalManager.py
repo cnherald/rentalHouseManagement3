48c314c3b37f69af046d9f99f81d00596933e879
cnherald <qliu040@aucklanduni.ac.nz>
2013-04-05 03:08:41 +1300

    commented three lines

--- src/rentalManager.py
+++ src/rentalManager.py
@@ -12,13 +12,13 @@ from datetime import datetime
 
 
 from google.appengine.ext.webapp import template
-from google.appengine.ext import webapp
-from google.appengine.ext import db
-from google.appengine.ext.webapp.util import run_wsgi_app
+#from google.appengine.ext import webapp
+#from google.appengine.ext import db
+#from google.appengine.ext.webapp.util import run_wsgi_app
 
 from django.utils import simplejson
 
-from datetime import datetime
+#from datetime import datetime
 import os, Cookie
 
 
