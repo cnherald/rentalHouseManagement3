4eb7a7942af150d22b74085941c16c94a0556dcd
Win7HomeMike <Win7HomeMike@mike.com>
2013-05-19 02:31:04 +1200

    comment out: #from google.appengine.dist import use_library #use_library('django','1.2')

--- src/rentalManager.py
+++ src/rentalManager.py
@@ -8,8 +8,8 @@
 #from google.appengine.ext import db
 #from google.appengine.ext.webapp.util import run_wsgi_app
 #from datetime import datetime
-from google.appengine.dist import use_library
-use_library('django','1.2')
+#from google.appengine.dist import use_library
+#use_library('django','1.2')
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from models import Tenants
