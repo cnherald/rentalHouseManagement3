b9dafbdd0ce5d9a5032b20605db8b08d123f354a
Win7HomeMike <Win7HomeMike@mike.com>
2013-07-08 23:04:51 +1200

    how to pass the tenant id to the http PUT request to the server?

--- src/rentalManager.py
+++ src/rentalManager.py
@@ -75,10 +75,12 @@ class RESTfulHandler(webapp.RequestHandler):
         tenant = simplejson.dumps(Tenants().registerTenant(inputData,key))
         self.response.out.write(tenant)
     
-    def put(self, tenantId):
+    #def put(self, tenantId):
+    def put(self):
         key = self.request.cookies['tenantlist']
         tenantlist = db.get(key)
-        tenant = Tenants.get_by_id(int(tenantId))
+        #tenant = Tenants.get_by_id(int(tenantId))
+        tenant = Tenants.get_by_id(int(7))
         if tenant.tenantlist.key() == tenantlist.key():           
             inputData = simplejson.loads(self.request.body)
 #            tenant.content = inputData['content']
